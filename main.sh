010b61c292a31cc80ea51b5f21dbe2e96a3558f5
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-11-15 11:06:22 +0000

    Update VPN check message

--- main.sh
+++ main.sh
@@ -27,7 +27,7 @@ GREEN='\033[0;32m'
 echo "1. System Update"
 echo "2. Update Rachel Interface"
 echo "3. Connect VPN"
-echo "4. Check VPN Connection"
+echo "4. Check VPN Status"
 echo "5. Download Rachel Logs"
 echo -e "${GREEN}6. Update Script"
 echo -e "${RED}7. Exit"
