1c0624d815d1e701f99ee04f2bec1701d513c8cd
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-11-15 00:13:52 +0000

    Update script to pull latest changes from GitHub

---
+++ update-script.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Save the current directory
+current_directory=$(pwd)
+
+# Move to the directory of the script
+cd "$(dirname "$0")" || exit
+
+# Pull the latest changes from GitHub
+git pull
+
+# Check if the pull was successful
+if [ $? -eq 0 ]; then
+    echo "Script updated successfully."
+    echo "Returning to the main script in 3 seconds..."
+else
+    echo "Failed to update the script. Please check for updates manually."
+fi
+
+# Return to the original directory
+cd "$current_directory"
+sleep 3
+exec ./main.sh
