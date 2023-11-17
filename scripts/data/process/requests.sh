eccd9c69e494ee402784cb6266c3b7683be1c4d7
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-11-17 17:29:19 +0000

    Refactor request folder processing in data processing script

--- scripts/data/process/requests.sh
+++ scripts/data/process/requests.sh
@@ -3,12 +3,13 @@
 # Set the path to your Python script
 python_script_path="scripts/data/process/processors/process_requests.py"
 
+echo ""
 # Prompt the user to pick a folder from '00_DATA' directory
 echo "Available requests folders in '00_DATA' directory:"
-folders=($(ls -d 00_DATA/*requests*/))  # Filter folders with "logs" in their name
+folders=($(ls -d 00_DATA/*requests*/))  # Filter folders with "requests" in their name
 
 if [ ${#folders[@]} -eq 0 ]; then
-    echo "No log folders found in '00_DATA'. Exiting..."
+    echo "No requests folders found in '00_DATA'. Exiting..."
     sleep 5
     exec ./scripts/data/process/main.sh
 fi
@@ -17,14 +18,20 @@ for ((i=0; i<${#folders[@]}; i++)); do
     echo "$((i+1)). ${folders[i]#00_DATA/}"
 done
 
+# variables
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+echo ""
 # Prompt the user for their choice
-read -p "Enter the number corresponding to the log folder you want to process: " folder_number
+read -p "Enter the number corresponding to the request folder you want to process: " folder_number
 
 # Validate user input
 if [[ ! $folder_number =~ ^[1-9][0-9]*$ || $folder_number -gt ${#folders[@]} ]]; then
-    echo "Invalid input. Please enter a valid folder number."
-    sleep 5
-    exec ./scripts/data/process/main.sh
+    echo ""
+    echo -e "${RED}Invalid input. Please enter a valid folder number."
+    echo -e "${NC}"
+    exec ./scripts/data/process/requests.sh
 fi
 
 # Construct the full path to the selected folder
@@ -34,13 +41,13 @@ selected_folder=${folders[$((folder_number-1))]#00_DATA/}
 python3 "$python_script_path" "$selected_folder"
 
 # Prompt the user for further action
-read -p "Do you want to process another log folder? (y/n): " user_response
+read -p "Do you want to process another request folder? (y/n): " user_response
 
 if [ "$user_response" == "y" ]; then
-    exec ./scripts/data/process/logs.sh
+    exec ./scripts/data/process/requests.sh
 else
-    echo "Returning to the main menu in 10 seconds..."
-    sleep 10
+    echo "Returning to the main menu in 5 seconds..."
+    sleep 5
     exec ./scripts/data/process/main.sh
-    # Add your logic for returning to the main menu or other actions
+    # Add your requestic for returning to the main menu or other actions
 fi
