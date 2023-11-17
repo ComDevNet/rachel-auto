2618d3b3103c0b67946c0145fd0686b871ceb186
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-11-17 15:45:43 +0000

    Refactor log folder selection in logs.sh script

--- scripts/data/process/logs.sh
+++ scripts/data/process/logs.sh
@@ -4,14 +4,21 @@
 python_script_path="scripts/data/process/processors/log.py"
 
 # Prompt the user to pick a folder from '00_DATA' directory
-echo "Available folders in '00_DATA' directory:"
-folders=($(ls -d 00_DATA/*/))
+echo "Available log folders in '00_DATA' directory:"
+folders=($(ls -d 00_DATA/*logs*/))  # Filter folders with "logs" in their name
+
+if [ ${#folders[@]} -eq 0 ]; then
+    echo "No log folders found in '00_DATA'. Exiting..."
+    sleep 5
+    exec ./scripts/data/process/main.sh
+fi
+
 for ((i=0; i<${#folders[@]}; i++)); do
     echo "$((i+1)). ${folders[i]#00_DATA/}"
 done
 
 # Prompt the user for their choice
-read -p "Enter the number corresponding to the folder you want to process: " folder_number
+read -p "Enter the number corresponding to the log folder you want to process: " folder_number
 
 # Validate user input
 if [[ ! $folder_number =~ ^[1-9][0-9]*$ || $folder_number -gt ${#folders[@]} ]]; then
@@ -32,7 +39,7 @@ read -p "Do you want to process another log folder? (y/n): " user_response
 if [ "$user_response" == "y" ]; then
     exec ./scripts/data/process/logs.sh
 else
-    echo "Returning to the main menu in 10 Seconds..."
+    echo "Returning to the main menu in 10 seconds..."
     sleep 10
     exec ./scripts/data/process/main.sh
     # Add your logic for returning to the main menu or other actions
