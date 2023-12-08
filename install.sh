8728f9931f947d171e3a210586a5bef308f9f79d
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2023-12-08 16:49:25 +0000

    Update install.sh script permissions

--- install.sh
+++ install.sh
@@ -9,15 +9,20 @@ wget https://raw.githubusercontent.com/xero/figlet-fonts/master/3d.flf
 sudo mv 3d.flf /usr/share/figlet/
 
 # install python3 and pip3
-pip install -r requirements.txt
+pip3 install -r requirements.txt
+
 
 # make the scripts executable
+sudo chmod +x *.sh
 sudo chmod +x scripts/vpn/*.sh
 sudo chmod +x scripts/update/*.sh
+sudo chmod +x scripts/system/*.sh
 sudo chmod +x scripts/data/*.sh
+sudo chmod +x scripts/data/all/*.sh
+sudo chmod +x scripts/data/all/process/*.sh
 sudo chmod +x scripts/data/collection/*.sh
 sudo chmod +x scripts/data/process/*.sh
-sudo chmod +x scripts/system/*.sh
-sudo chmod +x *.sh
+sudo chmod +x scripts/data/process/all/*.sh
+
 
 exec ./main.sh
