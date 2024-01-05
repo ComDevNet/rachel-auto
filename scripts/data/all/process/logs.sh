3f8acb526308671c886f92a89dd4852309f2fc7b
Llewellyn Paintsil <llewellynpaintsil34@gmail.com>
2024-01-05 10:13:16 +0000

    add processor selection to all

--- scripts/data/all/process/logs.sh
+++ scripts/data/all/process/logs.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-# Set the path to your Python script
-python_script_path="scripts/data/process/processors/log.py"
+# Script descriptions
+script1="V1 - For everyday Rachel Logs" 
+script2="V2 - For Cape Coast Castle Logs"
+
+# Display options with descriptions
+echo ""
+echo "Choose a Python script to run:"
+echo ""
+echo "1. $script1"
+echo "2. $script2"
+echo ""
+
+read -p "Enter the number corresponding to the script you want to run: " script_number
+
+# Validate user input
+if [[ ! $script_number =~ ^[1-2]$ ]]; then
+    echo "Invalid input. Please enter a valid script number."
+    sleep 3
+    exec ./scripts/data/process/main.sh
+fi
+
+# Set the path to the selected Python script
+if [ "$script_number" == "1" ]; then
+  python_script_path="scripts/data/process/processors/log.py"
+elif [ "$script_number" == "2" ]; then  
+  python_script_path="scripts/data/process/processors/logv2.py"
+fi
 
 echo ""
 # Prompt the user to pick a folder from '00_DATA' directory
@@ -9,9 +34,10 @@ echo "Select one of the available available log folders in '00_DATA' directory:"
 folders=($(ls -d 00_DATA/*logs*/))  # Filter folders with "logs" in their name
 
 if [ ${#folders[@]} -eq 0 ]; then
-    echo "No log folders found in '00_DATA'. Exiting..."
+    echo ""
+    echo "No log folders found in '00_DATA'. Processing Request file"
     sleep 5
-    exec ./scripts/data/process/main.sh
+    exec ./scripts/data/all/process/requests.sh
 fi
 
 for ((i=0; i<${#folders[@]}; i++)); do
@@ -19,11 +45,12 @@ for ((i=0; i<${#folders[@]}; i++)); do
 done
 
 # Prompt the user for their choice
+echo ""
 read -p "Enter the number corresponding to the log folder you want to process: " folder_number
 
 # Validate user input
 if [[ ! $folder_number =~ ^[1-9][0-9]*$ || $folder_number -gt ${#folders[@]} ]]; then
-    echo "Invalid input. Please enter a valid folder number."
+    echo "Invalid input. Please enter a valid folder number. Exiting..."
     sleep 5
     exec ./scripts/data/process/main.sh
 fi
--- scripts/data/process/all/logs.sh
+++ scripts/data/process/all/logs.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-# Set the path to your Python script
-python_script_path="scripts/data/process/processors/log.py"
+# Script descriptions
+script1="V1 - For everyday Rachel Logs" 
+script2="V2 - For Cape Coast Castle Logs"
+
+# Display options with descriptions
+echo ""
+echo "Choose a Python script to run:"
+echo ""
+echo "1. $script1"
+echo "2. $script2"
+echo ""
+
+read -p "Enter the number corresponding to the script you want to run: " script_number
+
+# Validate user input
+if [[ ! $script_number =~ ^[1-2]$ ]]; then
+    echo "Invalid input. Please enter a valid script number."
+    sleep 3
+    exec ./scripts/data/process/main.sh
+fi
+
+# Set the path to the selected Python script
+if [ "$script_number" == "1" ]; then
+  python_script_path="scripts/data/process/processors/log.py"
+elif [ "$script_number" == "2" ]; then  
+  python_script_path="scripts/data/process/processors/logv2.py"
+fi
 
 # Prompt the user to pick a folder from '00_DATA' directory
 echo "Available log folders in '00_DATA' directory:"
